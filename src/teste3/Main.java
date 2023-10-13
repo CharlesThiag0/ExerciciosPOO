86ac70b499abaff0d88968c637cbf4335242dd94
Charles <charlesthiagosantos@gmail.com>
2023-10-13 20:13:17 -0300

    atualização

--- src/teste3/Main.java
+++ src/teste3/Main.java
@@ -9,19 +9,18 @@ public class Main {
 		Disciplina disciplina = new Disciplina();
 		Curso curso = new Curso();
 		
-		//compossição de obj
-		turma1.setAluno(aluno);
+		turma1.setAluno(aluno);//compossição de obj
 		turma1.getAluno().setNome("Charles");
 		turma1.getAluno().setMatricula(141414);
 		turma1.getAluno().setNota1(9.0);
 		turma1.getAluno().setNota2(7.5);
 		
-		turma1.setDisciplina(disciplina);
+		turma1.setDisciplina(disciplina);//compossição de obj
 		turma1.getDisciplina().setNomeDaDisciplina("POO");
 		turma1.getDisciplina().setProfessor("Paulo");
 		turma1.getDisciplina().setCargaHoraria(5);
 		
-		turma1.setCurso(curso);
+		turma1.setCurso(curso);//compossição de obj
 		turma1.getCurso().setNomeDoCurso("Java");
 		turma1.getCurso().setCodigoDoCurso(2222);
 		
