482f3bc74cdef008d87500e6926c30aa9095dbef
Charles <charlesthiagosantos@gmail.com>
2023-10-16 13:30:38 -0300

    c13- correção e composição de obj

--- src/vendaDeCarro/Main.java
+++ src/vendaDeCarro/Main.java
@@ -5,6 +5,9 @@ public class Main {
 		Cliente cliente1 = new Cliente("Charles", "777.777.777-77", 22);
 		Carro carro1 = new Carro("Toyota", "Corola", 2019, 150000, 0);
 		DadosCompra compra1 = new DadosCompra(cliente1, carro1);
+		
+		compra1.setCarro(carro1);
+		compra1.setCliente(cliente1);
 
 		compra1.cupomDeDesconto(121212);
 		compra1.resultShow();
