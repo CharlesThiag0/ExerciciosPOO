ac7c941f2554aeab66603ed85cb902511de39a45
Charles <charlesthiagosantos@gmail.com>
2023-10-17 17:37:44 -0300

    c 25 - adição do projeto 90%

--- src/sobreCarros/Carro.java
+++ src/sobreCarros/Carro.java
@@ -1,5 +1,60 @@
 package sobreCarros;
 
 public class Carro {
+	private String modelo;
+	private int ano;
+	private double preco;
+
+	public Carro(String modelo, int ano, double preco) {
+
+		if (this.modelo != null) {
+			this.modelo = modelo;
+		} else {
+			this.modelo = "Gol";
+			System.out.println("O modelo não foi informado. Por isso usaremos " + this.modelo);
+		}
+
+		if (this.ano >= 1891) {
+			this.ano = ano;
+		} else {
+			this.ano = 2023;
+			System.out.println("Ano não localizado, determinamos como " + this.ano);
+		}
+
+		if (this.preco > 0) {
+			this.preco = preco;
+		} else {
+			this.preco = 12000;
+			System.out.println("Preço invalido, foi alterado para " + this.preco);
+		}
+	}
+
+	public Carro(String modelo, double preco) {
+		this(modelo, 2023, preco);
+	}
+
+	public String getModelo() {
+		return this.modelo;
+	}
+
+	public int getAno() {
+		return this.ano;
+	}
+
+	public double getPreco() {
+		return this.preco;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
 
 }
